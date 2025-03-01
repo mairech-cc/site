721fd98a8dd6728a889c0c36b5f2b8e1847b114b
JulMan <julman@julman.fr>
2025-03-02 00:46:40 +0100

    Pass github context to workflow script

--- .github/workflows/check_tsc.cjs
+++ .github/workflows/check_tsc.cjs
@@ -1,16 +1,18 @@
-const fs = require('fs');
+module.exports = ({ github }) => {
+  const fs = require('fs');
 
-const tscOutput = fs.readFileSync('tsc_output.txt', 'utf8').trim();
-const tscErrors = `### 🐞 TypeScript output\n\n~~~\n${tscOutput}\n~~~`;
+  const tscOutput = fs.readFileSync('tsc_output.txt', 'utf8').trim();
+  const tscErrors = `### 🐞 TypeScript output\n\n~~~\n${tscOutput}\n~~~`;
 
-const lintOutput = fs.readFileSync('eslint_output.txt', 'utf8').trim();
-const lintErrors = `### 🪄 ESLint output\n\n~~~\n${tscOutput}\n~~~`; lintOutput.includes("error") ? `### ❌ ESLint Errors\n\`\`\`\n${lintOutput}\n\`\`\`` : "✅ No ESLint errors found.";
+  const lintOutput = fs.readFileSync('eslint_output.txt', 'utf8').trim();
+  const lintErrors = `### 🪄 ESLint output\n\n~~~\n${tscOutput}\n~~~`; lintOutput.includes("error") ? `### ❌ ESLint Errors\n\`\`\`\n${lintOutput}\n\`\`\`` : "✅ No ESLint errors found.";
 
-const finalMessage = `${tscErrors}\n\n${lintErrors}`;
+  const finalMessage = `${tscErrors}\n\n${lintErrors}`;
 
-github.rest.issues.createComment({
-  issue_number: context.payload.pull_request.number,
-  owner: context.repo.owner,
-  repo: context.repo.repo,
-  body: finalMessage,
-});
+  github.rest.issues.createComment({
+    issue_number: context.payload.pull_request.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: finalMessage,
+  });
+}
