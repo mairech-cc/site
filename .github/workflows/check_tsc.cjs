a06f37be29e2bb3f696c61d4ae896ca3a8f1d810
JulMan <julman@julman.fr>
2025-03-02 04:12:31 +0100

    Fix output message formatting in checks

--- .github/workflows/check_tsc.cjs
+++ .github/workflows/check_tsc.cjs
@@ -4,10 +4,10 @@ module.exports = ({ github, context, root }) => {
   const fs = require("fs");
 
   const tscOutput = fs.readFileSync(path.resolve(root, "tsc_output.txt"), "utf8").trim();
-  const tscErrors = "### 🐞 TypeScript output" + tscOutput.trim() == "" ? "*No output*" : `\n\n~~~\n${tscOutput}\n~~~`;
+  const tscErrors = "### 🐞 TypeScript output" + (tscOutput.trim() == "" ? "*No output*" : `\n\n~~~\n${tscOutput}\n~~~`);
 
   const lintOutput = fs.readFileSync(path.resolve(root, "eslint_output.txt"), "utf8").trim();
-  const lintErrors = "### 🪄 ESLint output" + lintOutput.trim() == "" ? "*No output*" : `\n\n~~~\n${lintOutput}\n~~~`;
+  const lintErrors = "### 🪄 ESLint output" + (lintOutput.trim() == "" ? "*No output*" : `\n\n~~~\n${lintOutput}\n~~~`);
 
   const finalMessage = `🤖 *This is an automated message to ease code issues detection.*\n\n${tscErrors}\n\n${lintErrors}`;
 
