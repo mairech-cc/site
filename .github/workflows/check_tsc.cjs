b5fdd253dcf556a97837a9d80a235ea0c5b67146
JulMan <julman@julman.fr>
2025-03-02 04:17:00 +0100

    Enhance TypeScript and ESLint output messages

--- .github/workflows/check_tsc.cjs
+++ .github/workflows/check_tsc.cjs
@@ -4,12 +4,18 @@ module.exports = ({ github, context, root }) => {
   const fs = require("fs");
 
   const tscOutput = fs.readFileSync(path.resolve(root, "tsc_output.txt"), "utf8").trim();
-  const tscErrors = "### 🐞 TypeScript output" + (tscOutput.trim() == "" ? "*No output*" : `\n\n~~~\n${tscOutput}\n~~~`);
+  const tscErrors = "### 🐞 TypeScript output\n" + (tscOutput.trim() == "" ? "*No output*" : `~~~\n${tscOutput}\n~~~`);
 
   const lintOutput = fs.readFileSync(path.resolve(root, "eslint_output.txt"), "utf8").trim();
-  const lintErrors = "### 🪄 ESLint output" + (lintOutput.trim() == "" ? "*No output*" : `\n\n~~~\n${lintOutput}\n~~~`);
+  const lintErrors = "### 🪄 ESLint output\n" + (lintOutput.trim() == "" ? "*No output*" : `~~~\n${lintOutput}\n~~~`);
 
-  const finalMessage = `🤖 *This is an automated message to ease code issues detection.*\n\n${tscErrors}\n\n${lintErrors}`;
+  let finalMessage = `🤖 *This is an automated message to ease code issues detection.*\n\n${tscErrors}\n\n${lintErrors}`;
+
+  if (tscOutput.trim() == "" && lintOutput.trim() == "") {
+    finalMessage += "\n\n🎉 *No issues detected!*";
+  } else {
+    finalMessage += "\n\n🔍 *Please check the output above for more information. Note: Warnings are considered issues*";
+  }
 
   github.rest.issues.createComment({
     issue_number: context.payload.pull_request.number,
@@ -17,4 +23,10 @@ module.exports = ({ github, context, root }) => {
     repo: context.repo.repo,
     body: finalMessage,
   });
+
+  console.log("Commented on PR");
+
+  if (tscOutput.trim() != "" && lintOutput.trim() != "") {
+    throw new Error("Errors detected");
+  }
 }
