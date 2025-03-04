ebcd3f1342949aa7ef3614cfdcdc10c8990512fd
ShynyMp <shyny.mp06@gmail.com>
2025-03-04 17:12:55 +0100

    Ajout du son Ara Ara

--- src/modules/actions/list.ts
+++ src/modules/actions/list.ts
@@ -24,6 +24,7 @@ import animeWowMp3 from "../../assets/anime-wow.mp3";
 import enfantsDuRnMp3 from "../../assets/enfant-du-rn.mp3";
 import nootCenaMp3 from "../../assets/noot-cena.mp3";
 import uwuMp3 from "../../assets/uwu.mp3";
+import araAraMp3 from "../../assets/ara-ara.mp3";
 
 export type Action = {
   description: string;
@@ -236,5 +237,12 @@ export const ACTIONS: Record<string, Action> = {
     keys: ["u"],
     authors: ["Mathias"],
     action: ["play", uwuMp3],
+  },
+  araara: {
+    description: "Ara ara ~~~~",
+    type: "keys",
+    keys: ["Shift+a"],
+    authors: ["Mathias"],
+    action: ["play", araAraMp3],
   },
 };
